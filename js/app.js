ac4689c7f0f0b1fc628da659bd5c749b1695d84b
KyawThetOo007 <kthet4951@gmail.com>
2025-05-04 23:53:35 +0900

    third commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+JSON{
+    "name": "app",
+    "version": "1.0.0",
+    "description": "A simple JavaScript application",
+    "main": "index.js",
+    "scripts": {
+        "start": "node index.js"
+    },
+    "author": "Your Name",
+    "license": "ISC"
+}
